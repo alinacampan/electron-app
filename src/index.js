eca5c112b100f7cc7411fa214347030fc36e6264
Alina Campan <campana1@nku.edu>
2017-12-02 12:16:01 -0500

    Add finished game

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from 'electron';
+const { BrowserWindow, app } = require('electron');
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
